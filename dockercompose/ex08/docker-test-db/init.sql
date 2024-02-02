41218b745c3bfa23ced0b50cfeac8940aa648fab
minwoo <keuye06380618@gmail.com>
2024-02-03 03:58:56 +0900

    404 에러 html 추가

---
+++ dockercompose/ex08/docker-test-db/init.sql
@@ -0,0 +1,7 @@
+create table user_tb(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    name varchar(20)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO user_tb (name) values ('name1');
+INSERT INTO user_tb (name) values ('name2');
